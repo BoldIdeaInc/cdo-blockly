69c1898dcd99b44a9a52c1ceb24389296c3c7ccf
Ben Davis <bendavis78@gmail.com>
2016-05-08 09:48:48 -0500

    removed language-specfics from index.js

--- index.js
+++ index.js
@@ -8,9 +8,5 @@ Blockly.Msg = extend(require('./i18n/en'), Blockly.Msg);
 Blockly.Msg = Blockly.Msg();
 
 Blockly.Blocks = extend(Blockly.Blocks, require('./blocks_compressed')(Blockly));
-Blockly.JavaScript = require('./javascript_compressed')(Blockly);
-Blockly.PHP = require('./php_compressed')(Blockly);
-Blockly.Dart = require('./dart_compressed')(Blockly);
-Blockly.Python = require('./python_compressed')(Blockly);
 
 module.exports = Blockly;
